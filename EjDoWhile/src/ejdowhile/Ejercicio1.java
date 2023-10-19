12d69a79f97d58eefeec8652b5be7aff9d4d58cd
juanl2004 <145675461+juanl2004@users.noreply.github.com>
2023-10-19 12:10:44 +0200

    Add files via upload

---
+++ EjDoWhile/src/ejdowhile/Ejercicio1.java
@@ -0,0 +1,19 @@
+package ejdowhile;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+	
+		int num = 1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.println( + num);
+			num++;
+		} while (num<=20);
+	}
+
+}
