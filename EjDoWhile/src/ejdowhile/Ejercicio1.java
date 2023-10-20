9fb0f87e57482209a52be7f9c7c8839c43c299e0
juanl2004 <145675461+juanl2004@users.noreply.github.com>
2023-10-20 14:41:51 +0200

    Add files via upload

---
+++ EjDoWhile/src/ejdowhile/Ejercicio1.java
@@ -0,0 +1,17 @@
+package ejdowhile;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+	
+		int num = 1;
+		
+		do {
+			System.out.println( + num);
+			num++;
+		} while (num<=20);
+	}
+
+}
---
+++ EjDoWhile/src/ejdowhile/Ejercicio2.java
@@ -0,0 +1,15 @@
+package ejdowhile;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		int num = 0;
+		
+		do {
+			System.out.println("" + num);
+			num += 2;
+		} while (num<=200);
+		}
+
+	}
